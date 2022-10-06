f00a7b42dcc69191e585032f0b77e640de05e80e
Erfan Momeni <Erfan.Momeni@snapp.cab>
2022-10-06 15:32:53 +0330

    complete logic of some function

--- student_operations.py
+++ student_operations.py
@@ -41,12 +41,12 @@ def find_min(lis):
 
 def check_duplicate_code_melli(number) :
     for student in active_students :
-        if student[number] == student["code_melli"] :
+        if number == student["code_melli"] :
             return False
     return True
 def check_duplicate_student_code(number) :
     for student in active_students :
-        if student[number] == student["student_code"] :
+        if number == student["student_code"] :
             return False
     return True
 
@@ -57,12 +57,11 @@ def add_student() :
     student["last_name"] = input("Enter student last name : ")
     student["birthday"] = input("Enter student birthday (yyyy/mm/dd) : ")
 
-
     code_melli = input("Enter code melli (10 digit number) : ")
     if len(code_melli) != 10 or check_duplicate_code_melli(code_melli) == False :
         print("Code Melli is a 10 Unique Characters code like : xxxxxxxxxx")
         input("Press any key ...")
-        return 0
+        return 
     else :
         student["code_melli"] = code_melli
     
@@ -71,11 +70,11 @@ def add_student() :
     if len(student_code) != 5 or check_duplicate_student_code(student_code) == False :
         print("Student Code is a 5 Unique Characters code like : xxxxx")
         input("Press any key ...")
-        return 0
+        return 
     else :
         student["student_code"] = student_code
-        student["courses"] = list()
-        student["grades"]= list()
+    student["courses"] = list()
+    student["grades"]= list()
     while True:    
         student["courses"].append(input("Enter course name : "))
         student["grades"].append(int(input("Enter grades : ")))
@@ -83,9 +82,6 @@ def add_student() :
         if l == "N":
             break
     active_students.append(student)
-
-
-
 
 def find_student() :
     clear()
@@ -103,14 +99,10 @@ def find_student() :
             print("student age : ")
             print("--------------------------------------")
             input()
-            return True     
+            return     
     print("Not Found !")    
     input()
-    return False           
-
-
-
-
+    return          
 
 def delete_student() :
     clear()
@@ -139,7 +131,7 @@ def delete_student() :
                 print("Try again") 
                 return         
             input()
-            return True     
+            return   
     print("Not Found !")    
     input()
 def change_courses() :
@@ -147,12 +139,99 @@ def change_courses() :
     student_code = input("Enter student code to find student : ")
     for student in active_students :
         if student ["student_code"] == student_code :
+            l = input("Do you want to change first_name?(Y/N):")
+            if l == "Y" :
+                student["first_name"] = input("Enter student first name : ")
+            l = input("Do you want to change last_name?(Y/N):")
+            if l == "Y" :
+                student["last_name"] = input("Enter student last name : ")
+            l = input("Do you want to change birthday?(Y/N):")
+            if l == "Y":
+                student["birthday"] = input("Enter student birthday (yyyy/mm/dd) : ")
+            l = input("Do you want to change code melli?(Y/N):")
+            if l == "Y":
+                code_melli = input("Enter code melli (10 digit number) : ")
+                if len(code_melli) != 10 or check_duplicate_code_melli(code_melli) == False :
+                    print("Code Melli is a 10 Unique Characters code like : xxxxxxxxxx\nCode melli did not change")
+                else :
+                    student["code_melli"] = code_melli
+            l = input("Do you want to change student code?(Y/N):")
+            if l == "Y":
+                student_code = input("Enter student code (5 digit number) : ")
+                if len(student_code) != 5 or check_duplicate_student_code(student_code) == False :
+                    print("Student Code is a 5 Unique Characters code like : xxxxx\nstudent code did not change")
+                else :
+                    student["student_code"] = student_code
+            l = input("Do you want to add or remove grades?(Y/N)")
+            if l=="Y":  
+                l = input("Add or Remove?(A/R)")  
+                if l == "A":
+                    while True:    
+                        student["courses"].append(input("Enter course name : "))
+                        student["grades"].append(int(input("Enter grades : ")))
+                        l = input("Do you want to add more?(Y/N):")
+                        if l == "N":
+                            break
+                elif l == "R":
+                    course_input = input("Enter course name:")
+                    ind = 0
+                    for course in student["courses"]:
+                        if course == course_input:
+                            student["courses"].pop(ind)
+                            student["grades"].pop(ind)
+                            print("Done!")
+                            input()
+                            return 
+                    print("Not Found !")
+                    input()
+                    return 
+            print("Done!")        
             input()
-            //to do
-            return True     
+            return      
     print("Not Found !")    
     input()
+
 def list_students() :
-    pass
-def save_students() :
-    pass
+    print("-----------ACTIVE  STUDENTS-----------")
+    for student in active_students :
+        print(f"first name : {student['first_name']}")
+        print(f"last name : {student['last_name']}")
+        print(f"birthday : {student['birthday']}")
+        print(f"code melli : {student['code_melli']}")
+        print(f"student code : {student['student_code']}")
+        print(f"student average grade : {find_average(student['grades'])}")
+        print(f"student max grade : {find_max(student['grades'])}")
+        print(f"student min grade : {find_min(student['grades'])}")
+        print("student age : ")
+        print("--------------------------------------")
+    print("----------GRADUATED STUDENTS----------")
+    for student in graduated_students :
+        print(f"first name : {student['first_name']}")
+        print(f"last name : {student['last_name']}")
+        print(f"birthday : {student['birthday']}")
+        print(f"code melli : {student['code_melli']}")
+        print(f"student code : {student['student_code']}")
+        print(f"student average grade : {find_average(student['grades'])}")
+        print(f"student max grade : {find_max(student['grades'])}")
+        print(f"student min grade : {find_min(student['grades'])}")
+        print("student age : ")
+        print("--------------------------------------")
+    input("Press any key ...")
+
+def save_students() :    
+    from pickle import dump
+    try :
+        with open("ActiveStudents.db","wb") as  active_students_arr:
+            dump(active_students,active_students_arr)
+        with open("GraduatedStudents.db","wb") as graduated_students_arr :
+            dump(graduated_students,graduated_students_arr)
+            print("File has been saved succesfuly")
+            input("Press any key ...")
+    except(PermissionError) :
+        print("File can not be save due to Operating System Permissions")
+        input("Press any key ...")
+        return 
+    except :
+        print("Save Error !")
+        input("Press any key ...")
+        return
